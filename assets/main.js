7ae7db46bdf6aa58db3a4c7ce6e841e52ff1d097
Jose Padron F <josepadron.go@gmail.com>
2019-07-27 00:33:45 -0500

    share btns and disqus added

---
+++ assets/main.js
@@ -0,0 +1,7 @@
+(function() {
+  var d = document,
+    s = d.createElement("script");
+  s.src = "https://josepadrons-blog.disqus.com/embed.js";
+  s.setAttribute("data-timestamp", +new Date());
+  (d.head || d.body).appendChild(s);
+})();
